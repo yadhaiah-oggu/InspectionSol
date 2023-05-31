23c9edce38a08163c5506bfce020180736294e0c
Admin <Admin@YADHAIAHO>
2023-05-31 16:47:11 +0530

    Add project files.

---
+++ InspectionAPI/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InspectionAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DbContext> options):base(options) { }
+
+        public DbSet<Inspection> Inspections { get; set; }
+
+        public DbSet<InspectionType> InspectionTypes { get; set; }
+
+        public DbSet<Status> Statuses { get; set; }
+
+
+    }
+}
